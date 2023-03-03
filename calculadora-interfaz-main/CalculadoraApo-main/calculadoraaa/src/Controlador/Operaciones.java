16a0d750f38bdab56685dd2b27be7c49ee490e5a
sofiburbano12 <124370721+sofiburbano12@users.noreply.github.com>
2023-03-03 09:38:11 -0500

    Add files via upload

---
+++ calculadora-interfaz-main/CalculadoraApo-main/calculadoraaa/src/Controlador/Operaciones.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+/**
+ *
+ * @author sistemas
+ */
+public class Operaciones {
+    public String Sumar(float numero1, float numero2) {
+        float resultado  = numero1 + numero2;
+        return resultado+"";
+        //return resultado 
+    }
+    
+     public String Restar (float  numero1, float  numero2) {
+        float resultado  = numero1 - numero2;
+        return resultado+"";
+        //return resultado 
+    }
+      public String Multiplicacion (float numero1, float numero2) {
+        float resultado  = numero1 * numero2;
+        return resultado+"";
+        //return resultado 
+    }
+       public String Division (float numero1, float numero2) {
+        float resultado  = numero1 / numero2;
+        return resultado+"";
+        //return resultado 
+    }
+    
+}
